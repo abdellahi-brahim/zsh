d220efb872286211e4919884970f0db84f04fa2d
abdellahi-brahim <79083991+abdellahi-brahim@users.noreply.github.com>
2023-09-27 11:14:16 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,37 +1,26 @@
-# A. Install fontconfig for fc-cache
-sudo apt install -y fontconfig
+#!/bin/bash
 
-# B. Install Meslo Nerd Font for Powerlevel10k
-mkdir -p ~/.local/share/fonts
-cd ~/.local/share/fonts && curl -fLo "MesloLGS NF Regular.ttf" https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Regular.ttf
-cd ~/.local/share/fonts && curl -fLo "MesloLGS NF Bold.ttf" https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Bold.ttf
-cd ~/.local/share/fonts && curl -fLo "MesloLGS NF Italic.ttf" https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Italic.ttf
-cd ~/.local/share/fonts && curl -fLo "MesloLGS NF Bold Italic.ttf" https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Bold%20Italic.ttf
-fc-cache -f -v
+# Install Oh My ZSH!
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
-# C. Update and Install Zsh
-sudo apt update && sudo apt install -y zsh
+# Install Powerline fonts
+git clone https://github.com/powerline/fonts.git --depth=1
+cd fonts
+./install.sh
+cd ..
+rm -rf fonts
 
-# D. Install Oh My Zsh
-if [ ! -d "$HOME/.oh-my-zsh" ]; then
-  sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" --unattended
+# Set ZSH theme to agnoster and hide terminal context
+if ! grep -q "ZSH_THEME=\"agnoster\"" ~/.zshrc; then
+    echo "ZSH_THEME=\"agnoster\"" >> ~/.zshrc
 fi
 
-# E. Install Powerlevel10k theme
-[ ! -d "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k" ] && git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+if ! grep -q "prompt_context(){}" ~/.zshrc; then
+    echo "prompt_context(){}" >> ~/.zshrc
+fi
 
-# F. Install zsh-autosuggestions for autocomplete
-[ ! -d "${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions" ] && git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+# Set Visual Studio Code configurations for ZSH (Optional)
+# echo "\"terminal.integrated.fontFamily\": \"Source Code Pro for Powerline\"," >> ~/.vscode/settings.json
+# echo "\"terminal.integrated.shell.osx\": \"/bin/zsh\"" >> ~/.vscode/settings.json
 
-# G. Enable zsh-autosuggestions
-grep -q 'zsh-autosuggestions.zsh' ~/.zshrc || echo 'source $ZSH_CUSTOM/plugins/zsh-autosuggestions/zsh-autosuggestions.zsh' >> ~/.zshrc
-
-# H. Set Powerlevel10k and zsh-autosuggestions as the default theme and plugin in .zshrc
-grep -q 'ZSH_THEME="powerlevel10k/powerlevel10k"' ~/.zshrc || sed -i.bak 's/^ZSH_THEME=".*"/ZSH_THEME="powerlevel10k/powerlevel10k"/' ~/.zshrc
-grep -q 'plugins=(git zsh-autosuggestions)' ~/.zshrc || echo 'plugins=(git zsh-autosuggestions)' >> ~/.zshrc
-
-# I. Change default shell to zsh
-chsh -s $(which zsh)
-
-# J. Source .zshrc to apply changes
-[ "$0" = "zsh" ] && source ~/.zshrc || echo 'Please restart your terminal or run "source ~/.zshrc" to apply changes.'
+echo "Configuration applied! Restart your terminal to see the changes."
